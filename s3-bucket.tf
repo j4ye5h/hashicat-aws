645338079631d9eaed2710858995e62b1c598903
jayesh <jayesh.patel@icloud.com>
2021-06-14 11:14:40 +0000

    Added s3 bucket module4

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-657778474747"
   #bucket_prefix = "10.0.10.0/24"
   acl    = "private"
 
